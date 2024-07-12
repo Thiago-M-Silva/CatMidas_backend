cbce0842ed936b28d3f47a685a62bab4e34eea49
Thiago <thiagobossun06@gmail.com>
2024-07-12 10:59:05 -0300

    Conclusao da criacao de teste

--- src/test/java/com/example/catalogo/Controllers/AnimeControllerTest.java
+++ src/test/java/com/example/catalogo/Controllers/AnimeControllerTest.java
@@ -6,17 +6,39 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.example.catalogo.Desenho.Desenho;
+import com.example.catalogo.Desenho.DesenhoRepository;
+import com.example.catalogo.Desenho.DesenhoRequestDTO;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.DisplayName;
 
 @DataJpaTest
 @ActiveProfiles("test")
 class AnimeControllerTest {
+    /*A ideia e simular o contexto de uso da funcao em questao, no caso do setAll() e necessario criar um registro no banco de dados e 
+    chamar a funcao de busca por id (que no caso, o alvo dos testes). Criamos a funcao createDesenho para criar esse registro e usaar 
+    a funcao de busca.
+    Caso houvesse dependencias, seria necessario utilizar mockito (@mock, beforeeach) para simular os retornos
+    */
 
     @Autowired
     EntityManager entityManager;
 
+    @Autowired
+    DesenhoRepository desenhoRepository;
+
     @Test
+    @DisplayName("Should return anime from bd")
     void setAll() {
+        DesenhoRequestDTO data = new DesenhoRequestDTO(null, "teste", "teste", "teste","teste", 
+                                                            "teste", 0, null, "teste", "a", "teste", 0);
+        this.createDesenho(data);
+        Optional<Desenho> resultado = this.desenhoRepository.findById(1L);
+        assertThat(resultado.isPresent()).isTrue();
     }
 
     @Test
@@ -31,7 +53,9 @@ class AnimeControllerTest {
     void updateAnime() {
     }
 
-    private desenho createDesenho(Anime anime){
-        //min 17:29
+    private Desenho createDesenho(DesenhoRequestDTO data){
+        Desenho newDesenho = new Desenho(data);
+        this.entityManager.persist(newDesenho);
+        return newDesenho;
     }
 }
